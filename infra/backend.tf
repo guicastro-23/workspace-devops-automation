f59dfd14902bce4913943085d460201d0d3bfb9c
Guilherme de Oliveira Castro <guilhermegcastro20@gmail.com>
2026-03-02 16:51:00 -0300

    Lab - Terraform + Ansible com botão de Destroy

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatecurso10092"
+    storage_account_name = "tfstatecurso14807"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
